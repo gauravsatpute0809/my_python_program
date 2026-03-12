50bed6c9fb98d646ab56d06eab1c03a6eca0cddb
gauravsatpute0809 <satputegaurav42@gmail.com>
2026-03-12 20:48:16 +0530

    Python : vowels and consonent

---
+++ Check vowel or consonant.py
@@ -0,0 +1,7 @@
+alphabet = input("Enter a alphabet to check vowels or consonant : ")
+
+if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u'or alphabet == 'A' or alphabet == 'E' or alphabet == 'I' or alphabet == 'O' or alphabet == 'U':
+    print("alphabet are vowels")
+    
+else:
+    print("alphabet are consonent..") 
