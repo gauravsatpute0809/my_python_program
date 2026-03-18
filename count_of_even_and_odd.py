152988a02097c840b54d26b47be6757bbad14766
gauravsatpute0809 <satputegaurav42@gmail.com>
2026-03-18 20:59:21 +0530

    Python : Even and Odd count

---
+++ count_of_even_and_odd.py
@@ -0,0 +1,19 @@
+
+        
+num = int(input("Enter a Number: "))
+
+counteven = 0
+countodd = 0
+
+while num > 0:
+    digit = num % 10   # extract last digit
+
+    if digit % 2 == 0:
+        counteven += 1
+    else:
+        countodd += 1
+
+    num = num // 10    # remove last digit
+
+print("Even digits:", counteven)
+print("Odd digits:", countodd)
